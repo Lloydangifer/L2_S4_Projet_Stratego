bffca2e54283dbf5ab0222b493514005618120c2
Lloydangifer <vmanrique@netcourrier.com>
2015-05-04 15:09:52 +0200

    maj

--- play.c
+++ play.c
@@ -1,1 +1,6 @@
 #include "play.h"
+
+void move_piece();
+void select_piece();
+void delete_piece();
+void verif_move();
--- play.h
+++ play.h
@@ -1,6 +1,9 @@
 #ifndef PLAY_H
 #define PLAY_H
 
-#endif
+void move_piece();
+void select_piece();
+void delete_piece();
+void verif_move();
 
-void move_piece(struct game *stratego,)
+#endif
