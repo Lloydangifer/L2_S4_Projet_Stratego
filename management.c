bc5073cde6b9e6bd7b5af09ad03585a11c0a4cbc
Darkness-S <sammyloudiyi@gmail.com>
2015-05-15 17:55:11 +0200

    a

--- management.c
+++ management.c
@@ -1,1 +1,41 @@
 #include "management.h"
+//function to check the position of player pieces
+void check_player_piece(int piece*, int player){
+	int=0;	
+	for (i=0; i<39; i++){
+		//If one of the cell is empty, it send KO to the player and exit
+		if (pieces[i]=NULL){
+			write(player, "KO", 3);
+			return EXIT_FAILURE;
+		}
+	}
+	write(player, "OK", 3);
+}
+
+//function to check player move. Return "N"(Nothing) if it work, or "B"(Battle)
+void check_move(char *playerMove){
+	}
+
+//Make turn, at 0.5 of interval
+void turn(int player0, int player1, char *buf){
+	char player0Move=read(player0,buf,10);
+	//Check if the player move is empty. If it is, return an error "E", and the other player win.
+	if (strcmp(player0Move,"\0"){
+		write(player0,"E",2);
+		write(player1,"F",2);
+		printf("Player 1 win");
+		return EXIT_FAILURE;
+	}
+	check_move(player1Move);
+	char player1Move=read(player1,buf,10);
+	if (strcmp(player1Move,"\0"){
+		write(player1,"E",2);
+		write(player0,"F",2);
+		printf("Player 0 win");
+		return EXIT_FAILURE;
+	}
+	check_move(player0Move);
+	alarm(0.5);
+}
+	
+	
--- referee.c
+++ referee.c
@@ -4,71 +4,96 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #define BUFSIZE 1024  
+
 int main(int argc, char **argv){
 	if(argc!=3){
 		fprintf(stderr,"The program need two argument. "); //The referee need three arguments (him and the two players). If there's a different number argument, the programs exit.
 		return EXIT_FAILURE;
 	}
+	//We open the log file (or create it if they don't exist)
+	int log0 = creat("player0.log",S_IRWXU|S_IRGRP|S_IROTH);
+	int log1 = creat("player1.log",S_IRWXU|S_IRGRP|S_IROTH);
 
+	//char bufGood[BUFSIZE]="OK"; //Buffer for the message display if there's no error
+	//char bufBad[BUFSIZE]="KO"; //Buffer for the message display if there's an error
+	
+	//Path of player 0
+	char play0Path[BUFSIZE];
+	sprintf(play0Path,"%s",argv[1]);
+	printf("Path Player 0 : %s\n",play0Path);
+	//Path of player 1
+	char play1Path[BUFSIZE];
+	sprintf(play1Path,"%s",argv[2]);
+	printf("Path Player 1: %s\n",play1Path);
+	
+	printf("Player 0 :%s\nPlayer 1 : %s\n",argv[1], argv[2]); //Display the player. The first player in the arguments is Player 0, and the seconde player in the arguments is Player 1
+	
 	int play0In[2]; //File descriptor for the input of player 0
 	pipe(play0In);	//Pipe of the file descriptor above
 	int play0Out[2]; //File descriptor for the output of player 0
 	pipe(play0Out); //Pipe of the file descriptor above
+	
+	//Fork player 0
+	if(fork()==0){
+		//We redirect the standard input and stadard output, then we close the pipe
+		dup2(play0In[0],0); 
+		dup2(play0Out[1],1);
+		dup2(log0, 2);
+		close(play0In[1]);
+		close(play0Out[0]);
+		close(play0In[0]);
+		close(play0Out[1]);
+		//close(log0);
+		//We execute the program of Player 0. If there's a problem --> EXIT_FAILURE
+		execv(play0Path,argv);
+		return EXIT_FAILURE;
+	}
+	//We close unused pipe
+	close(play0In[0]);
+	close(play0Out[1]);
+
 	int play1In[2]; //File descriptor for the input of player 1
 	pipe(play1In); //Pipe of the file descriptor above
 	int play1Out[2]; //File descriptor for the output of player 1
 	pipe(play1Out); //Pipe of the file descriptor above
-
-	char bufGood[BUFSIZE]="OK"; //Buffer for the message display if there's no error
-	char bufBad[BUFSIZE]="KO"; //Buffer for the message display if there's an error
 	
-	char play0Path[BUFSIZE];
-	sprintf(play0Path,"%s",argv[1]);
-	printf("Path : %s\n",play0Path);
-	char play1Path[BUFSIZE];
-	sprintf(play1Path,"%s",argv[2]);
-	printf("Path : %s\n",play1Path);
-	
-	printf("Player 0 :%s\nPlayer 1 : %s\n",argv[1], argv[2]); //Display the player. The first player in the arguments is Player 0, and the seconde player in the arguments is Player 1
-	
+	//Fork Player 1
 	if(fork()==0){
-		// Joueur 0
-		close(play1In[0]);
+		//We redirect the standard input and stadard output, then we close the pipe
+		dup2(play1In[0],0);
+		dup2(play1Out[1],1);
+		dup2(log1,2);
 		close(play1In[1]);
 		close(play1Out[0]);
+		close(play1In[0]);
 		close(play1Out[1]);
-		dup2(play0In[0],STDIN_FILENO); 
-		dup2(play0Out[1],STDOUT_FILENO);
-		execv(play0Path,argv);
-	}else{
-		if(fork()==0){
-			// Joueur 1
-
-			close(play0In[0]);
-			close(play0In[1]);
-			close(play1Out[0]);
-			close(play1Out[1]);
-			dup2(play1In[0],STDIN_FILENO);
-			dup2(play1Out[1],STDOUT_FILENO);
-			execv(play1Path,argv);
-		}
-		// Attente du joueur 0
- 
-		write(play0In[1],bufGood,3);
- 
-		wait(NULL);
-		read(play0Out[0],bufGood,3);
-		printf("Message reçu = \"%s\"\n", bufGood);
-
-		// Attente du joueur 1
-
-		write(play1In[1],bufBad,3);
-
-		wait(NULL);
-		read(play1Out[0],bufBad,3);
-		printf("Message reçu = \"%s\"\n", bufBad);
+		//We execute the program of Player 1. If there's a problem --> EXIT_FAILURE
+		execv(play1Path,argv);
+		return EXIT_FAILURE;
 	}
+	//We close unused pipe
+	close(play1In[0]);
+	close(play1Out[1]);
+
+	int play0Input=play0In[1];
+	int play0Output=play0Out[0];
+	int play1Input=play1In[1];
+	int play1Output=play1Out[0];
+	//We give to the players their number
+	write(play0Input,"0",2);
+	write(play1Input,"1",2);
+	wait(NULL);
+	//Change SIGALARM to the function  turn
+	signal(SIGALRM, turn);
+	//Set alarm to 0.5s
+	alarm(0.5);
+	//We close the log file
+	close(log0);
+	close(log1);
     return EXIT_SUCCESS;
 }
